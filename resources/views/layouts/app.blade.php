213e8fa89881cb449550dd45e8cc9e3522271ce3
Erik Camacho <skarmorysteel@gmail.com>
2024-07-21 19:18:03 -0600

    feat: Added menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,15 +1,18 @@
 <!DOCTYPE html>
-<html lang='en'>
+<html lang='es'>
     <head>
         <meta charset='utf-8'>
         <title>Pokémon App - @yield('title')</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
     </head>
     <body>
-        <div class='content'>
+        @include('layouts.menu')
+        <main>
             @yield('content')
-        </div>
+        </main>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
+        <script src="{{ asset('js/script.js') }}"></script>
     </body>
 </html>
---
+++ resources/views/layouts/menu.blade.php
@@ -0,0 +1,22 @@
+<!-- Menu -->
+<nav class="navbar navbar-expand-md navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a href="/" class="text-title fs-2">Pokémon APP</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#lateral-menu">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <section class="offcanvas offcanvas-start bg-dark" id="lateral-menu" tabindex="-1">
+            <div class="offcanvas-header" data-bs-theme="dark">
+                <h5 class="offcanvas-title text-title">Pokémon APP</h5>
+                <button class="btn-close" type="button" aria-label="Close" data-bs-dismiss="offcanvas"></button>
+            </div>
+            <div class="offcanvas-body d-flex flex-column">
+                <ul class="navbar-nav fs-5 justify-content-evenly">
+                    <li class="nav-item p-3 py-md-1"><a class="nav-link" href="/">App</a></li>
+                    <li class="nav-item p-3 py-md-1"><a class="nav-link" href="/contact">Contacto</a></li>
+                </ul>
+            </div>
+        </section>
+    </div>
+</nav>
+<!-- End menu -->
